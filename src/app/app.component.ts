d985c7315442e759044ec3659112c56914a6bb0e
mateuszkalwaj <mateusz.kalwaj@gmail.com>
2025-03-25 16:21:54 +0100

    Small refactor (extracted header component)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {ParkingDataService} from './services/parking-data/parking-data.service';
 import {ParkingListComponent} from './components/parking-list/parking-list.component';
+import {HeaderComponent} from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   imports: [
-    ParkingListComponent
+    ParkingListComponent,
+    HeaderComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
