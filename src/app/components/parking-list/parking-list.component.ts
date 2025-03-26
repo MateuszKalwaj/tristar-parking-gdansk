cc0010643e18bf1261665db497907ce699969d46
mateuszkalwaj <mateusz.kalwaj@gmail.com>
2025-03-26 13:41:18 +0100

    Added conditional styling for parking cards and sorted data

--- src/app/components/parking-list/parking-list.component.ts
+++ src/app/components/parking-list/parking-list.component.ts
@@ -1,16 +1,15 @@
 import {Component, OnInit} from '@angular/core';
 import {ParkingDataService} from '../../services/parking-data/parking-data.service';
-// import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-parking-list',
   standalone: true,
-  // imports: [CommonModule], TODO make sure it works ONLY with @ngFor and not @for
   templateUrl: './parking-list.component.html',
   styleUrls: ['./parking-list.component.css']
 })
 export class ParkingListComponent implements OnInit {
   parkingData: any[] = [];
+
 
   constructor(private parkingDataService: ParkingDataService) {
   }
@@ -18,8 +17,15 @@ export class ParkingListComponent implements OnInit {
   ngOnInit(): void {
     this.parkingDataService.getParkingList().subscribe(parkingData => {
       this.parkingDataService.getParkingAvailability().subscribe(availabilityData => {
-        this.parkingData = this.parkingDataService.combineParkingData(parkingData, availabilityData);
+        this.parkingData = this.sortParkingData(
+          this.parkingDataService.combineParkingData(parkingData, availabilityData));
       });
+    });
+  }
+
+  private sortParkingData(parkingData: any[]): any[] {
+    return parkingData.sort((a, b) => {
+      return (b.availableSpots > 0 ? 1 : 0) - (a.availableSpots > 0 ? 1 : 0);
     });
   }
 }
