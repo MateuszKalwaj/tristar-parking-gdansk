b6b31043b1cecc514c0ce01044759a72de306df0
mateuszkalwaj <mateusz.kalwaj@gmail.com>
2025-03-27 17:09:02 +0100

    Change path to env

--- src/app/components/parking-list/parking-list.component.ts
+++ src/app/components/parking-list/parking-list.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {ParkingDataService} from '../../services/parking-data/parking-data.service';
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import {environment} from '../../../environments/environment';
+import {environment} from 'src/environments/environment';
 
 @Component({
   selector: 'app-parking-list',
